be84b38b1fe97a0bd1586e0bc868cd3f9ec34a06
AgustinMarceloRemeta <89120120+AgustinMarceloRemeta@users.noreply.github.com>
2023-08-15 00:47:32 -0300

    LevelsSO

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-    [SerializeField]List<LevelSO> levels;
-    [SerializeField] List<Button> buttons;
+    [SerializeField] GameObject buttonsParent;
+    public LevelSO[] levels;
+    public Button[] buttons;
 
     private void Start()
     {
-        for (int i = 0; i < levels.Count; i++)
+        levels = Resources.LoadAll<LevelSO>("LevelS");
+        buttons = buttonsParent.GetComponentsInChildren<Button>();
+        for (int i = 0; i < levels.Length; i++)
         {
-            buttons[i].image.sprite = levels[i].sprites[PlayerPrefs.GetInt("Level" + levels[i].level)];
-            buttons[i].onClick.AddListener(() => SceneManager.LoadScene("Level"+ i)) ;
+            int levelActual = levels[i].level;
+            int progress = PlayerPrefs.GetInt("Level" + levelActual);
+            if (progress != 0) buttons[i].image.sprite = levels[i].sprites[progress-1];
+            else buttons[i].image.sprite = levels[i].defaultSprite;
+            buttons[i].onClick.AddListener(() => SceneManager.LoadScene("Level"+levelActual));
         }
     }
 }
